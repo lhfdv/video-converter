8bc1ec3c7e4bf638023b015a7e87ffbafb0729ca
lhfdv <lhfranco@protonmail.com>
2023-06-11 01:21:35 -0300

    convert through commands

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import shutil
 import subprocess
-import urllib.parse
+import sys
+import argparse
+import uvicorn
+from typing import Optional
 
 from fastapi import FastAPI, Request, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
@@ -19,26 +22,13 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/convert")
-async def convert(
+async def convert_web(
     request: Request,
     media_file: UploadFile = Form(...),
     output_format: str = Form(...)
 ):
     try:
-        if output_format.lower() not in SUPPORTED_FORMATS:
-            raise ValueError("Invalid output format.")
-
-        media_filename = media_file.filename
-        media_path = f"uploads/{media_filename}"
-        with open(media_path, "wb") as buffer:
-            shutil.copyfileobj(media_file.file, buffer)
-
-        converted_filename = f"{os.path.splitext(media_filename)[0]}.{output_format}"
-        converted_path = f"uploads/{converted_filename}"
-        command = f'ffmpeg -i "{media_path}" "{converted_path}"'
-
-        subprocess.run(command, shell=True, check=True)
-
+        converted_filename = convert_file(media_file.file, media_file.filename, output_format)
         return templates.TemplateResponse(
             "result.html",
             {"request": request, "converted_filename": converted_filename},
@@ -49,6 +39,47 @@ async def convert(
             {"request": request, "error": str(e)},
         )
 
+def convert_file(file, filename, output_format):
+    if output_format.lower() not in SUPPORTED_FORMATS:
+        raise ValueError("Invalid output format.")
+
+    media_filename = filename
+    media_path = f"uploads/{media_filename}"
+    with open(media_path, "wb") as buffer:
+        shutil.copyfileobj(file, buffer)
+
+    converted_filename = f"{os.path.splitext(media_filename)[0]}.{output_format}"
+    converted_path = f"uploads/{converted_filename}"
+    command = f'ffmpeg -i "{media_path}" "{converted_path}"'
+
+    subprocess.run(command, shell=True, check=True)
+
+    return converted_filename
+
+def convert_cli(input_file: str, output_format: str, output_file: Optional[str] = None):
+    if output_file is None:
+        output_file = f"{os.path.splitext(input_file)[0]}.{output_format}"
+    
+    # Extract the filename from the full file path
+    input_filename = os.path.basename(input_file)
+
+    with open(input_file, "rb") as file:
+        convert_file(file, input_filename, output_format)
+
+    print(f"Conversion complete! The file is ready: {output_file}")
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    parser = argparse.ArgumentParser(description="Media Converter CLI")
+    parser.add_argument("-i", "--input_file", help="Path to the input media file")
+    parser.add_argument("-o", "--output_format", help="Output format for conversion")
+    parser.add_argument("-of", "--output_file", help="Path to the output file (optional)")
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        # No command-line arguments provided, start the web server
+        import uvicorn
+        uvicorn.run(app, host="0.0.0.0", port=8000)
+    else:
+        # Command-line arguments provided, perform conversion
+        convert_cli(args.input_file, args.output_format, args.output_file)
